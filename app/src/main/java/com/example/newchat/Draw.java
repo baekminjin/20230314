698cb2f0f8327fa508f4dd51856d088d7e134aa3
minjin <bhs3392@naver.com>
2023-03-14 13:22:46 +0900

    닉네임과 채팅

---
+++ app/src/main/java/com/example/newchat/Draw.java
@@ -0,0 +1,102 @@
+package com.example.newchat;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.Nullable;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+public class Draw extends AppCompatActivity {
+
+    MyPaintView view;
+    int tColor,n=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+        view = new MyPaintView(this);
+
+
+        LinearLayout container = findViewById(R.id.container);
+        Resources res = getResources();
+
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        container.addView(view, params);
+
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+
+
+        Button btn=findViewById(R.id.colorPickerButton);
+        Button btn2=findViewById(R.id.thickPickerButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorPicker();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show();
+            }
+        });
+    }
+
+    private void show() {
+        final EditText editText=new EditText(this);
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("AlertDialog Title");
+        builder.setMessage("굵기 입력");
+        builder.setView(editText);
+        builder.setPositiveButton("입력",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        view.setStrokeWidth(Integer.parseInt(editText.getText().toString()));
+
+                    }
+                });
+        builder.setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.show();
+
+    }
+
+    private void openColorPicker() { //색상 선택
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, tColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+            }
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                Toast.makeText(getApplicationContext(),""+tColor,Toast.LENGTH_LONG).show();
+                view.setColor(color);
+            }
+        });
+        colorPicker.show();
+    }
+
+}
